18fd84f782a90feab3e5fe96496b52a08cf9d848
Vergel25B <bayonavergel1@gmail.com>
2022-03-10 19:23:08 +0800

    dont activity 3

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,6 +1,23 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
+
+
+export const commonColor = {
+  main: '#F66B5E',
+  secondary: '#015193',
+  white: '#fff',
+  black: '#191620',
+  transparent: '#0000',
+  lightBlue: '#88B4FF',
+  lightBG: '#F9F9F9',
+  dark: '#28292D',
+  green: '#5DC16E',
+  lgihtGreen: '#90EE90',
+  textColor: '#0957DE',
+  textDark: '#878787'
+}
+
 export default {
   light: {
     text: '#000',
